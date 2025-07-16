053da936e271b0d48418711438beb17c71b31001
theonlydola <muhammeddadel@gmail.com>
2025-07-16 22:08:42 +0300

    feat: connect mongodb & add user schema

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [UsersModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost:27017/users_db'),
+    UsersModule,
+  ],
 })
 export class AppModule {}
